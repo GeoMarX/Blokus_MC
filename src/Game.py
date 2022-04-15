fa534b86b89e214e99256c0163b7a18ff577ccdd
brahimdriss <59141044+brahimdriss@users.noreply.github.com>
2022-04-15 18:41:39 +0200

    Updated game file

--- src/Game.py
+++ src/Game.py
@@ -1,5 +1,6 @@
 import random
 from copy import deepcopy
+import math
 
 class Game:
     """
@@ -91,7 +92,7 @@ class Game:
                 # place the player at the back of the queue
                 
                 self.h = self.h ^ self.get_hash(proposal,current.idx)
-                self.history.append(move)
+                self.history.append(proposal)
                 first = (self.players).pop(0)
                 self.players = self.players + [first]
                 # increment the number of rounds just played
@@ -102,6 +103,50 @@ class Game:
         
         else:
             print ("Game over! And the winner is: " + self.winner().name)
+    
+    def play_AMAF(self,random=False):
+
+        if self.rounds == 0:
+            max_x = ((self.board).size[1] - 1)
+            max_y = ((self.board).size[0] - 1)
+            starts = [(0, 0), (max_y, max_x), (0, max_x), (max_y, 0)]
+            
+            for i in range(len(self.players)):
+                (self.players[i]).add_pieces(self.all_pieces)
+                (self.players[i]).start_corner(starts[i])
+        
+        if self.winner() == "None":
+            current = self.players[0]
+            if random:
+                proposal = current.do_random_move(self)    
+            else:
+                proposal = current.do_move(self)
+            
+            if proposal == None:
+                first = (self.players).pop(0)
+                self.players = self.players + [first]
+                self.rounds += 1
+            
+            
+
+            elif self.valid_move(current, proposal.points):
+                (self.board).update(current, proposal.points)
+                current.update_player(proposal, self.board)
+                current.remove_piece(proposal)
+                
+                self.h = self.h ^ self.get_hash(proposal,current.idx)
+
+                first = (self.players).pop(0)
+                self.players = self.players + [first]
+                self.history.append(move)
+                self.rounds += 1
+            
+            else: raise Exception("Invalid move by " + current.name + ".")
+        
+        else:
+            print ("Game over! And the winner is: " + self.winner().name)
+
+            return proposal
 
     def play_move(self,move,random=False):
         """
@@ -131,11 +176,14 @@ class Game:
             else:
                 proposal = move
             
+            self.history.append(proposal)            
+
             if proposal == None:
                 # move on to next player, increment rounds
                 first = (self.players).pop(0)
                 self.players = self.players + [first]
                 self.rounds += 1
+            
             
             
             # ensure that the proposed move is valid
@@ -149,10 +197,10 @@ class Game:
                 # place the player at the back of the queue
                 
                 self.h = self.h ^ self.get_hash(proposal,current.idx)
+                
 
                 first = (self.players).pop(0)
                 self.players = self.players + [first]
-                self.history.append(move)
                 # increment the number of rounds just played
                 self.rounds += 1
             
@@ -174,13 +222,67 @@ class Game:
         self.play(random=True)
         while self.winner() == "None":
             self.play(random=True)
+            
+        return self.winner()
 
-        # print("The final scores are...")
+    def playoutAMAF (self, played):
 
-        # by_name = sorted(self.players, key = lambda player: player.name)
+        move = self.play_AMAF(random=True)
+        if move is not None:
+            played.append(self.get_hash(move, self.players[0].idx))
+        while self.winner() == "None":
+            move = self.play_AMAF(random=True)
+            if move is not None:
+                played.append(self.get_hash(move, self.players[0].idx))
+        return self.winner()
 
-        # for p in by_name:
-        #     print (p.name + " : " + str(p.score))
-        
+    def playout_PPA(self,policy):
+        current = self.players[0]
+        shape_options = [p for p in current.pieces]
+        l = current.possible_moves(shape_options, self) 
+        zlist = []
+        zlist2 = []
+        if len(l) != 0:
+            z = 0
+            for i in range (len (l)):
+                z = z + math.exp (policy.get(self.get_hash(l[i], current.idx),0))
+                zlist.append(z)
+            stop = random.random () * z
+            move = 0
+            z = 0
+            while True:
+                z = z + math.exp (policy.get(self.get_hash(l[move], current.idx),0))
+                zlist2.append(z)
+                if z >= stop:
+                    break
+                move = move + 1
+            self.play_move (l [move])
+
+        else:
+            self.play_move (None)
+
+        while self.winner() == "None":
+            current = self.players[0]
+            shape_options = [p for p in current.pieces]
+            l = current.possible_moves(shape_options, self)
+            zlist3 = []
+            zlist4 = []
+            if len(l) != 0:
+                z = 0
+                for i in range (len (l)):
+                    z = z + math.exp (policy.get(self.get_hash(l[i], current.idx),0))
+                    zlist3.append(z)
+                stop = random.random () * z
+                move = 0
+                z = 0
+                while True:
+                    z = z + math.exp (policy.get(self.get_hash(l[move], current.idx),0))
+                    zlist4.append(z)
+                    if z >= stop:
+                        break
+                    move = move + 1
+                self.play_move (l[move])
+            else:
+                self.play_move (None)
         
         return self.winner()
